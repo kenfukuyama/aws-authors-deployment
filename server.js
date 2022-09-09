8bf5a1404798414a98379d4d266ec6183ea2193a
Ken Fukuyama <kenfukuyama0112@gmail.com>
2022-09-09 11:05:37 -0700

    build proess

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ require('./server/config/mongoose.config');
 
 
 app.use(cors()) 
+
+// this middle ware enables the json response using post requsest
 app.use(express.json());
 app.use(express.json(), express.urlencoded({ extended: true }));
 // require('./server/routes/person.routes')(app); // This is new
 // require('./server/routes/product.routes')(app); // This is new
+
 require('./server/routes/author.routes')(app); // This is new
     
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
